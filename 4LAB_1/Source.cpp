be227d21394ddad462761d9474a1304b0a4b6b12
leti9302 <adsemenov@stud.etu.ru>
2021-02-20 18:53:53 +0300

    4Lab_1

--- 4LAB_1/Source.cpp
+++ 4LAB_1/Source.cpp
@@ -6,18 +6,10 @@ void main()
 	rb_tree->insert(33, 1);
 	rb_tree->insert(13, 2);
 	rb_tree->insert(53, 3);
-	rb_tree->insert(11, 4);
-	rb_tree->insert(21, 5);
-	rb_tree->insert(41, 6);
-	list<int> keys = rb_tree->get_keys();
-	cout << keys.back();
-	//rb_tree->insert(33, 1);
-	//rb_tree->insert(13, 2);
-	//rb_tree->insert(53, 3);
-	//std::list <int> keys;
-	//std::list <int> values;
-	//keys = rb_tree->get_keys();
-	//values = rb_tree->get_values();
-	//rb_tree->print();
-	//delete rb_tree;
+	std::list <int> keys;
+	std::list <int> values;
+	keys = rb_tree->get_keys();
+	values = rb_tree->get_values();
+	rb_tree->print();
+	delete rb_tree;
 }
